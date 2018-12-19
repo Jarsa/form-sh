4fe5d8d71c1ef6988404b85fa72ae8c84797e2f7
Alan Ramos <alan.ramos@jarsa.com.mx>
2018-12-18 18:23:59 -0600

    [ADD] form: repository initialization

---
+++ form/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Form Instance',
+    'summary': 'Module that install Form Instance',
+    'version': '12.0.1.0.0',
+    'category': 'Customs',
+    'author': 'Jarsa Sistemas, Vauxoo, Odoo Community Association (OCA)',
+    'website': 'https://www.jarsa.com.mx',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_accountant',
+        'l10n_mx',
+        'purchase',
+        'sale',
+        'mrp_plm',
+    ],
+}
