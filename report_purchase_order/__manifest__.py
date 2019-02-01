c322e202667b61cd9a0e84084786ca9d824f1709
Isabel Esparza <isabel.esparza@jarsa.com.mx>
2019-02-01 15:04:16 -0600

    [ADD][report_purchase_order]Module to custom PO report Task#1713

---
+++ report_purchase_order/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lpgl.html).
+
+{
+    'name': 'Report purchase order for FORM',
+    'external_dependencies': {
+        'python': [
+            'num2words',
+        ],
+    },
+    'summary': 'Custom purchase order report ',
+    'version': '12.0.1.0.0',
+    'category': 'Report',
+    'author': 'Jarsa Sistemas',
+    'website': 'https://www.jarsa.com.mx',
+    'license': 'LGPL-3',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_template.xml',
+        'views/purchase_order_view.xml',
+    ],
+}
