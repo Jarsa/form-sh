af89c1c2667774ce1341f8d96fa61a91d26be32d
Alan Ramos <alan.ramos@jarsa.com.mx>
2019-02-27 04:13:45 -0500

    [ADD] form_instance: add security group to allow to modify certain fields to user mrp_manager

---
+++ form_instance/models/product_template.py
@@ -0,0 +1,20 @@
+# Copyright 2019, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.multi
+    def write(self, vals):
+        if self.user_has_groups(
+                'form_instance.group_edit_product_mrp_user'):
+            allowed_fields = ['cost_price', 'tracking']
+            for key in vals.keys():
+                if key not in allowed_fields:
+                    raise ValidationError(
+                        _('You are not allowed to modify products.'))
+        return super().write(vals)
