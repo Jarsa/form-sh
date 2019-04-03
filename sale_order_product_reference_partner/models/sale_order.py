121dd1ba9decde3c5b65e46bf687e04a467593e5
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-04-03 07:00:55 -0600

    [PATCH] sale_order_product_reference_partner: comment code to remove ValidationError while the data is defined by the customer

--- sale_order_product_reference_partner/models/sale_order.py
+++ sale_order_product_reference_partner/models/sale_order.py
@@ -1,8 +1,7 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, models, _
-from odoo.exceptions import ValidationError
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
@@ -27,19 +26,20 @@ class SaleOrderLine(models.Model):
 
     @api.model
     def _check_customer_product(self):
-        if self.product_id:
-            if not self.order_id.partner_id:
-                raise ValidationError(
-                    _('You has not selected a Customer to Sale Order'))
-            if not self.product_id.partner_ids:
-                raise ValidationError(
-                    _('The product %s have not asiggned any customer')
-                    % self.product_id.name)
-            if self.order_id.partner_id not in self.product_id.partner_ids:
-                raise ValidationError(
-                    _('The Customer %s is different to have'
-                        ' the product %s')
-                    % (
-                        self.order_id.partner_id.name,
-                        self.product_id.name)
-                    )
+        # if self.product_id:
+        #     if not self.order_id.partner_id:
+        #         raise ValidationError(
+        #             _('You has not selected a Customer to Sale Order'))
+        #     if not self.product_id.partner_ids:
+        #         raise ValidationError(
+        #             _('The product %s have not asiggned any customer')
+        #             % self.product_id.name)
+        #     if self.order_id.partner_id not in self.product_id.partner_ids:
+        #         raise ValidationError(
+        #             _('The Customer %s is different to have'
+        #                 ' the product %s')
+        #             % (
+        #                 self.order_id.partner_id.name,
+        #                 self.product_id.name)
+        #             )
+        return True
