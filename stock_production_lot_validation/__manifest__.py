a23cb27be39ef53e375a733c4047dfea0ea1168b
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-04-20 17:06:28 -0500

    [ADD] stock_production_lot_validation: new module to validate name of the lot

---
+++ stock_production_lot_validation/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019, Jarsa Sistemas S.A de C.V
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Lot Validation',
+    'summary': "Validate the name of the lot when it's created",
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'website': 'https://www.jarsa.com.mx',
+    'author': 'Jarsa Sistemas',
+    'license': 'LGPL-3',
+    'installable': True,
+    'depends': [
+        'stock',
+    ],
+}
