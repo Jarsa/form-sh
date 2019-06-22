b7ab596b2830ba4496e3fe7effe4653f17a68350
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-06-22 11:54:13 -0500

    [FIX] stock_picking_name_reference_change: show correct default code an add quantities

--- stock_picking_name_reference_change/models/mrp_production.py
+++ stock_picking_name_reference_change/models/mrp_production.py
@@ -11,9 +11,11 @@ class MrpProduction(models.Model):
     def create(self, values):
         res = super().create(values)
         product_name = res.product_tmpl_id.name
+        default_code = res.product_tmpl_id.default_code
+        units = '%s %s' % (res.product_qty, res.product_uom_id.name)
         if res.product_tmpl_id.description_purchase:
             product_name = res.product_tmpl_id.description_purchase
         for rec in res.picking_ids:
-            rec.origin = '%s - [%s] %s' % (
-                rec.origin, rec.product_id.default_code, product_name)
+            rec.origin = '%s - [%s] %s - %s' % (
+                rec.origin, default_code, product_name, units)
         return res
