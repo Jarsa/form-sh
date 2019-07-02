98a31541a5c501deb2b1a9962aace11f66a71d2a
lmgr0312 <lmiguel.gruiz@gmail.com>
2019-07-02 10:57:24 -0500

    [ADD][Stock Automatic Lot]

---
+++ stock_automatic_lot/README.rst
@@ -0,0 +1,10 @@
+Lot Auto Assign Name
+====================
+
+This module create automatically a lot when a Product's IN picking is validate, and when a product is manufactured.
+Credits
+=======
+
+**Contributors**
+* Miguel Ruiz <miguel.ruiz@jarsa.com.mx> (Developer)
+* Jarsa Sistemas SA de CV <info@jarsa.com.mx>
---
+++ stock_automatic_lot/wizards/mrp_product_produce.py
@@ -0,0 +1,22 @@
+# Copyright 2019, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class MrpProductProduce(models.TransientModel):
+    _inherit = 'mrp.product.produce'
+
+    @api.multi
+    def do_produce(self):
+        lot_obj = self.env['stock.production.lot']
+        picking_obj = self.env['stock.picking']
+        active_model = self._context['active_model']
+        lot_sequence = picking_obj._get_lot_sequence(active_model)
+        lot = lot_obj.create({
+            'name': lot_sequence.next_by_id(),
+            'product_id': self.product_id.id,
+            'product_qty': self.product_qty})
+        self.lot_id = lot.id
+        return super().do_produce()
