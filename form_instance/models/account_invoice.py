ae7fd9ce1e28720aa45ff154422b92725e1026a6
jesus01x <xzan12@outlook.com>
2019-07-12 18:45:57 -0500

    [ADD] account_invoice_line: add group that It not allow edit invoice lines

--- form_instance/models/account_invoice.py
+++ form_instance/models/account_invoice.py
@@ -1,7 +1,8 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class AccountInvoice(models.Model):
@@ -23,3 +24,23 @@ class AccountInvoice(models.Model):
         lines = self.invoice_line_ids.filtered(lambda l: l.quantity == 0.0)
         self.invoice_line_ids -= lines
         return res
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = "account.invoice.line"
+
+    @api.multi
+    def write(self, values):
+        name_user = self.env.user.name
+        fields_blocked = ['quantity', 'price_unit', 'invoice_line_tax_ids']
+        if not self.user_has_groups(
+                'form_instance.group_allow_edit_invoices'):
+            for rec in fields_blocked:
+                if rec in values:
+                    raise UserError(
+                        _(
+                            'The user %s can not permission to edit the price'
+                            'unit, quantity and taxes'
+                        ) % name_user
+                    )
+        return super().write(values)
