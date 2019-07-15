63a25ba6cf9e8586bf2ac36dbf48b1cae2010ac4
jesus01x <xzan12@outlook.com>
2019-07-15 17:14:54 +0000

    [ADD] stock_picking: add field price unit and subtotal to view of stock picking

--- form_instance/models/stock_move.py
+++ form_instance/models/stock_move.py
@@ -1,7 +1,7 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -10,9 +10,29 @@ class StockMove(models.Model):
     product_supplier_name = fields.Text(
         related='product_id.description_purchase')
 
+    type_operation = fields.Selection(
+        related='picking_id.picking_type_id.code',
+        store=True)
+    subtotal = fields.Float(compute='_compute_subtotal', store=True)
+
+    @api.multi
+    def _compute_subtotal(self):
+        for rec in self:
+            rec.subtotal = rec.quantity_done * rec.price_unit
+
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     product_supplier_name = fields.Text(
         related='product_id.description_purchase')
+    type_operation_line = fields.Selection(
+        related='move_id.type_operation',
+        store=True)
+    price_unit = fields.Float(related='move_id.price_unit')
+    subtotal = fields.Float(compute='_compute_subtotal')
+
+    @api.multi
+    def _compute_subtotal(self):
+        for rec in self:
+            rec.subtotal = rec.qty_done * rec.price_unit
