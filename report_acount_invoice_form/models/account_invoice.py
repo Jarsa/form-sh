b122a2287710e2d4ee0853b4a6bd30eb62e1b5f4
IsabelEsparza <isa_tkm_ris@live.com.mx>
2019-07-22 14:57:58 -0500

    [FIX][report_acount_invoice_form]changue method amount to text and get rate

--- report_acount_invoice_form/models/account_invoice.py
+++ report_acount_invoice_form/models/account_invoice.py
@@ -1,7 +1,16 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lpgl.html).
 
+import logging
+
 from odoo import api, models
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from num2words import num2words
+except ImportError as err:
+    _logger.debug(err)
 
 
 class AccountInvoice(models.Model):
@@ -20,7 +29,7 @@ class AccountInvoice(models.Model):
             val_tc = 0.0
             val_tc += rec.currency_id.with_context(
                 date=rec.date_invoice).rate
-        return val_tc
+        return 1/val_tc
 
     @api.multi
     def get_tax(self):
@@ -37,3 +46,22 @@ class AccountInvoice(models.Model):
             for k, v in taxes_names.items():
                 dict_taxes[k] = sum(v)
         return dict_taxes
+
+    @api.multi
+    def _amount_to_text(self):
+        self.ensure_one()
+        currency_name = self.currency_id.name.upper()
+        currency_type = 'M.N' if currency_name == 'MXN' else 'M.E.'
+        currency = 'PESOS' if currency_name == 'MXN' else 'DOLARES'
+        amount_i, amount_d = divmod(self.amount_total, 1)
+        amount_d = round(amount_d, 2)
+        amount_d = int(round(amount_d * 100, 2))
+        words = (
+            num2words(
+                float(amount_i), lang=self.partner_id.lang or 'es_ES').upper())
+        invoice_words = (
+            '%(words)s %(currency)s  %(amount_d)02d/100 %(curr_t)s' %
+            dict(
+                words=words, currency=currency,
+                amount_d=amount_d, curr_t=currency_type))
+        return invoice_words
