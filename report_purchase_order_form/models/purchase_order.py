9bcec44f9ab67d967597339030c4238fb6d59641
IsabelEsparza <isa_tkm_ris@live.com.mx>
2019-07-25 18:30:18 -0500

    [FIX][report_purchase_order_form]Change design

--- report_purchase_order_form/models/purchase_order.py
+++ report_purchase_order_form/models/purchase_order.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lpgl.html).
 
 import logging
-from odoo import models
+from odoo import api, models
 
 _logger = logging.getLogger(__name__)
 try:
@@ -14,18 +14,21 @@ except ImportError as err:
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    def _amount_to_text(
-            self, amount, currency, partner_lang='es_MX'):
-        total = str(float(amount)).split('.')[0]
-        decimals = str(float(amount)).split('.')[1]
-        currency_type = 'M.N.'
-        if partner_lang != 'es_MX':
-            total = num2words(float(amount)).upper()
-        else:
-            total = num2words(float(total), lang='es').upper()
-        if currency != 'MXN':
-            currency_type = 'M.E.'
-        else:
-            currency = 'PESOS'
-        return '%s %s %s/100 %s' % (
-            total, currency, decimals or 0.0, currency_type)
+    @api.multi
+    def _amount_to_text(self):
+        self.ensure_one()
+        currency_name = self.currency_id.name.upper()
+        currency_type = 'M.N' if currency_name == 'MXN' else 'M.E.'
+        currency = 'PESOS' if currency_name == 'MXN' else 'DOLARES'
+        amount_i, amount_d = divmod(self.amount_total, 1)
+        amount_d = round(amount_d, 2)
+        amount_d = int(round(amount_d * 100, 2))
+        words = (
+            num2words(
+                float(amount_i), lang=self.partner_id.lang or 'es_ES').upper())
+        invoice_words = (
+            '%(words)s %(currency)s  %(amount_d)02d/100 %(curr_t)s' %
+            dict(
+                words=words, currency=currency,
+                amount_d=amount_d, curr_t=currency_type))
+        return invoice_words
