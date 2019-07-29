f393a8893c8e72557bdc3065780489e9d09816e7
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-07-29 12:11:33 -0500

    [FIX] form_instance: hide workorders when the raw materials are note available

--- form_instance/models/mrp_workorder.py
+++ form_instance/models/mrp_workorder.py
@@ -1,11 +1,17 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import models
 
 
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
-    availability = fields.Selection(
-        related='production_id.availability')
+    def do_finish(self):
+        action = super().do_finish()
+        action['domain'] = [
+            ('state', 'not in', ['done', 'cancel', 'pending']),
+            ('workcenter_id', '=', self.workcenter_id.id),
+            ('production_availability', '=', 'assigned'),
+        ]
+        return action
