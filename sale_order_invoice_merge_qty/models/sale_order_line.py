860af576b5b94cf1c5000e47148c50b489919ab0
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-09-12 14:09:48 -0500

    [FIX] sale_order_invoice_merge_qty: fix issue that computes incorrectly the qty invoiced

--- sale_order_invoice_merge_qty/models/sale_order_line.py
+++ sale_order_invoice_merge_qty/models/sale_order_line.py
@@ -7,7 +7,18 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.depends('invoice_lines.invoice_id.state', 'invoice_lines.quantity',
-                 'invoice_lines.sibling_invoice_id.state')
+    @api.depends('invoice_lines.invoice_id.state', 'invoice_lines.quantity')
     def _get_invoice_qty(self):
-        return super()._get_invoice_qty()
+        for line in self:
+            qty_invoiced = 0.0
+            for invoice_line in line.invoice_lines:
+                if invoice_line.invoice_id.state != 'cancel':
+                    if len(invoice_line.sale_line_ids) > 1:
+                        qty_invoiced = line.qty_delivered
+                    elif invoice_line.invoice_id.type == 'out_invoice':
+                        qty_invoiced += invoice_line.uom_id._compute_quantity(
+                            invoice_line.quantity, line.product_uom)
+                    elif invoice_line.invoice_id.type == 'out_refund':
+                        qty_invoiced -= invoice_line.uom_id._compute_quantity(
+                            invoice_line.quantity, line.product_uom)
+            line.qty_invoiced = qty_invoiced
