229521d6a13e1a0cac004730f2418fec89fe8718
lmgr0312 <lmiguel.gruiz@gmail.com>
2020-01-15 12:40:45 -0600

    [ADD[Account Payment]Super on account payment create to be able of change sequence in payment when is created through bank statement

--- form_instance/models/account_payment.py
+++ form_instance/models/account_payment.py
@@ -11,3 +11,22 @@ class AccountPayment(models.Model):
         string='Reference',
         related='partner_id.ref',
     )
+
+    def create(self, vals):
+        if vals['partner_type']:
+            sequence_code = ''
+            if vals['partner_type'] == 'customer':
+                if vals['payment_type'] == 'inbound':
+                    sequence_code = 'account.payment.customer.invoice'
+                if vals['payment_type'] == 'outbound':
+                    sequence_code = 'account.payment.customer.refund'
+            elif vals['partner_type'] == 'supplier':
+                if vals['payment_type'] == 'inbound':
+                    sequence_code = 'account.payment.supplier.refund'
+                if vals['payment_type'] == 'outbound':
+                    sequence_code = 'account.payment.supplier.invoice'
+            vals['name'] = self.env['ir.sequence'].with_context(
+                ir_sequence_date=vals['payment_date']).next_by_code(
+                sequence_code)
+        res = super().create(vals)
+        return res
