5de4e66508fa751d41727c7b6de6bed50d396afd
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-17 13:17:57 -0600

    [ADD] account_aged_partner_balance_by_currency: Module to add currency in aged payable and receivable reports

---
+++ account_aged_partner_balance_by_currency/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2020, Jarsa Sistemas
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import models
---
+++ account_aged_partner_balance_by_currency/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2020, Jarsa Sistemas
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import account_aged_partner
+from . import report_aged_partner_balance
