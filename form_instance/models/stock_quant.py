2d5f1afefea0ee754b60f6126a5f2310d9b55149
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-07-03 17:27:32 -0500

    [FIX] form_instance: add decimal precision to quant quantity and reserved quantity fields

--- form_instance/models/stock_quant.py
+++ form_instance/models/stock_quant.py
@@ -2,11 +2,23 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
 
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
+    quantity = fields.Float(
+        'Quantity',
+        digits=dp.get_precision('Product Unit of Measure'),
+        help='Quantity of products in this quant, in the default unit of measure of the product',
+        readonly=True, required=True, oldname='qty')
+    reserved_quantity = fields.Float(
+        'Reserved Quantity',
+        digits=dp.get_precision('Product Unit of Measure'),
+        default=0.0,
+        help='Quantity of reserved products in this quant, in the default unit of measure of the product',
+        readonly=True, required=True)
     product_supplier_name = fields.Text(
         related='product_id.description_purchase')
     categ_id = fields.Many2one(
