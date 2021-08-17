5dec3963023182825b746006f39d9461ef23fb3d
IbraimFerniza <ibraim.ferniza@jarsa.com.mx>
2021-08-17 10:22:33 -0500

    [MIG] report_acount_invoice_form: migrate module task#8546

--- report_acount_invoice_form/models/account_move.py
+++ report_acount_invoice_form/models/account_move.py
@@ -13,13 +13,13 @@ except ImportError as err:
     _logger.debug(err)
 
 
-class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
     def get_picking(self):
-        if self.origin:
+        if self.name:
             so = self.env['sale.order'].search(
-                [('name', '=', self.origin)])
+                [('name', '=', self.name)])
             return so.warehouse_id.name
 
     def get_rate(self):
@@ -43,21 +43,3 @@ class AccountInvoice(models.Model):
             for k, v in taxes_names.items():
                 dict_taxes[k] = sum(v)
         return dict_taxes
-
-    def _amount_to_text(self):
-        self.ensure_one()
-        currency_name = self.currency_id.name.upper()
-        currency_type = 'M.N' if currency_name == 'MXN' else 'USD'
-        currency = 'PESOS' if currency_name == 'MXN' else 'DOLARES'
-        amount_i, amount_d = divmod(self.amount_total, 1)
-        amount_d = round(amount_d, 2)
-        amount_d = int(round(amount_d * 100, 2))
-        words = (
-            num2words(
-                float(amount_i), lang=self.partner_id.lang or 'es_ES').upper())
-        invoice_words = (
-            '%(words)s %(currency)s  %(amount_d)02d/100 %(curr_t)s' %
-            dict(
-                words=words, currency=currency,
-                amount_d=amount_d, curr_t=currency_type))
-        return invoice_words
