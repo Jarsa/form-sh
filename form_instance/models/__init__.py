ca3a961a74242538f24157ae9fcfa55059598b17
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-09-01 13:00:53 -0500

    [REM] form_instance: remove reconciliation widget
    It is not longer required

--- form_instance/models/__init__.py
+++ form_instance/models/__init__.py
@@ -16,7 +16,6 @@ from . import mrp_workorder
 from . import product_template
 from . import purchase_order
 from . import quality_alert
-# from . import reconciliation_widget
 # from . import res_partner
 # from . import sale_order
 # from . import stock_inventory
