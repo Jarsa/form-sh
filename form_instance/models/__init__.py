1f03cf2cf85014d2782034cc4f4879727b78db47
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-09-06 08:20:30 -0500

    [MIG] form_instance: changes in stock.move and stock.move.line
    Add reserved quantity to stock.move.line report
    Add product_supplier name field in stock.move and stock.move.line
    Add subtotal in stock.move and stock.move.line
    Add type of operation of picking and price unit in stock.move.line
    Assign dest to extra lines created
    When a stock move is assigned it checks related moves to assign them.

--- form_instance/models/__init__.py
+++ form_instance/models/__init__.py
@@ -19,7 +19,7 @@ from . import quality_alert
 from . import res_partner
 from . import sale_order
 from . import stock_inventory
-# from . import stock_move
+from . import stock_move
 # from . import stock_picking_type
 # from . import stock_quant
 # from . import uom_uom
