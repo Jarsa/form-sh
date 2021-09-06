ab770c9fb334931eacd9c64720c02a1b5736b434
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-09-06 08:24:33 -0500

    [MIG] form_instance: add chatter to stock.picking.type model

--- form_instance/models/__init__.py
+++ form_instance/models/__init__.py
@@ -20,6 +20,6 @@ from . import res_partner
 from . import sale_order
 from . import stock_inventory
 from . import stock_move
-# from . import stock_picking_type
+from . import stock_picking_type
 # from . import stock_quant
 # from . import uom_uom
