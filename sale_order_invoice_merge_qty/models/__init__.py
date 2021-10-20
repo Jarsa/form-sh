abb5297bdc7e32bf177b394726cf7dcc9058c3c2
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-10-20 14:18:09 -0500

    [FIX] sale_order_invoice_merge_qty: allow to post invoices

--- sale_order_invoice_merge_qty/models/__init__.py
+++ sale_order_invoice_merge_qty/models/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lpgl.html).
 
+from . import account_move
 from . import account_move_line
 from . import sale_order_line
 from . import sale_order
