f4acfe9ea884de00e204eb9a6eab77c70a05a12f
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-10-31 01:32:15 -0500

    [FIX] form_instance: update migration script to consider stock move and stock move line in equivalence

--- form_instance/migrations/14.0.1.0.1/post-migration.py
+++ form_instance/migrations/14.0.1.0.1/post-migration.py
@@ -15,7 +15,7 @@ to_remove = [
 
 def process_equivalence(env):
     products = env['product.template'].search([
-        ('equivalent_factor', '!=', False),
+        ('equivalent_factor', '!=', [False, 1.0, 0.0]),
         ('equivalent_uom_id', '!=', False),
     ])
     count = 1
@@ -44,22 +44,22 @@ def process_equivalence(env):
             _logger.warning('Order lines processed %s of %s' % (po_count, len(order_lines)))
             po_count += 1
             line._onchange_product_qty_purchase_order_secondary_unit()
-        # move_lines = env['stock.move.line'].search([
-        #     ('product_id.product_tmpl_id', '=', product.id),
-        # ])
-        # _logger.warning('Processing %s stock move lines' % (len(move_lines)))
-        # move_lines.with_context(do_not_unreserve=True).write({
-        #     'secondary_uom_id': secondary_unit.id,
-        # })
-        # move_lines.with_context(do_not_unreserve=True)._compute_qty_done()
-        # moves = env['stock.move'].search([
-        #     ('product_id.product_tmpl_id', '=', product.id),
-        # ])
-        # _logger.warning('Processing %s stock move lines' % (len(moves)))
-        # moves.with_context(do_not_unreserve=True).write({
-        #     'secondary_uom_id': secondary_unit.id,
-        # })
-        # moves.with_context(do_not_unreserve=True)._compute_product_uom_qty()
+        move_lines = env['stock.move.line'].search([
+            ('product_id.product_tmpl_id', '=', product.id),
+        ])
+        _logger.warning('Processing %s stock move lines' % (len(move_lines)))
+        move_lines.with_context(do_not_unreserve=True).write({
+            'secondary_uom_id': secondary_unit.id,
+        })
+        move_lines.with_context(do_not_unreserve=True)._compute_qty_done()
+        moves = env['stock.move'].search([
+            ('product_id.product_tmpl_id', '=', product.id),
+        ])
+        _logger.warning('Processing %s stock move lines' % (len(moves)))
+        moves.with_context(do_not_unreserve=True).write({
+            'secondary_uom_id': secondary_unit.id,
+        })
+        moves.with_context(do_not_unreserve=True)._compute_product_uom_qty()
 
 
 @openupgrade.migrate()
