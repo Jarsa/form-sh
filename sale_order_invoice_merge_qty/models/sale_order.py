3bc972348d4219a5d163777385bc0f18c522dd7b
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-11-05 18:49:48 -0600

    [FIX] sale_order_invoice_merge_qty: fix issue with unbalanced entries generated by deleting account.move.line one by one, it was fixed with dictionary in write

--- sale_order_invoice_merge_qty/models/sale_order.py
+++ sale_order_invoice_merge_qty/models/sale_order.py
@@ -11,17 +11,22 @@ class SaleOrder(models.Model):
     def _merge_line_qty(self, moves):
         for move in moves:
             products = move.invoice_line_ids.mapped('product_id')
+            lines_to_update = []
             for product in products:
                 lines = move.invoice_line_ids.filtered(
                     lambda l: l.product_id.id == product.id)
                 if len(lines) > 1:
-                    lines[0].with_context(check_move_validity=False).write({
+                    lines_to_update.append((1, lines[0].id, {
                         'quantity': sum(lines.mapped('quantity')),
                         'sale_line_ids': [
                             (6, 0, lines.mapped('sale_line_ids').ids)],
-                    })
-                    lines[1:].unlink()
-            move._onchange_invoice_line_ids()
+                    }))
+                    lines_to_update.extend([(2, line.id, 0) for line in lines[1:]])
+            if lines_to_update:
+                move.write({
+                    'invoice_line_ids': lines_to_update,
+                })
+                move._onchange_invoice_line_ids()
 
     def _create_invoices(self, grouped=False, final=False, date=None):
         moves = super()._create_invoices(grouped, final, date)
