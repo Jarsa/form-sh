09b588abc24de350730c343cd5b21ff810add795
HBM-GWCG <genaro@gwcg.mx>
2025-10-22 14:51:40 -0600

    modulos v17

--- form_instance/models/stock_inventory.py
+++ form_instance/models/stock_inventory.py
@@ -5,31 +5,31 @@ from odoo import fields, models
 
 
 class StockInventory(models.Model):
-    _inherit = 'stock.inventory.adjustment'
+    _inherit = 'stock.inventory.warning'
 
-    def _get_inventory_lines_values(self):
-        vals = super()._get_inventory_lines_values()
-        product_model = self.env['product.product']
-        for val in vals:
-            product = product_model.browse(val['product_id'])
-            val['cost'] = product.standard_price
-        return vals
+    # def _get_inventory_lines_values(self):
+    #     vals = super()._get_inventory_lines_values()
+    #     product_model = self.env['product.product']
+    #     for val in vals:
+    #         product = product_model.browse(val['product_id'])
+    #         val['cost'] = product.standard_price
+    #     return vals
 
-    def action_validate(self):
-        lines = self.line_ids
-        for line in lines:
-            teorical_qty = line.theoretical_qty
-            real_qty = line.product_qty
-            line_cost = line.cost
-            product_cost = line.product_id.standard_price
-            if (teorical_qty != real_qty and line_cost != product_cost):
-                line.product_id.write({
-                    'standard_price': line_cost
-                })
-        return super().action_validate()
+    # def action_validate(self):
+    #     lines = self.line_ids
+    #     for line in lines:
+    #         teorical_qty = line.theoretical_qty
+    #         real_qty = line.product_qty
+    #         line_cost = line.cost
+    #         product_cost = line.product_id.standard_price
+    #         if (teorical_qty != real_qty and line_cost != product_cost):
+    #             line.product_id.write({
+    #                 'standard_price': line_cost
+    #             })
+    #     return super().action_validate()
 
 
 class StockInventoryLine(models.Model):
-    _inherit = 'stock.inventory.adjustment.line'
+    _inherit = 'stock.inventory.warning'
 
     cost = fields.Float()
