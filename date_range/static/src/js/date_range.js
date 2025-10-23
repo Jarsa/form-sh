0e95a29c149741133c8fd1543a50418272f15ad7
HBM-GWCG <genaro@gwcg.mx>
2025-10-22 20:16:53 -0600

    modulos v17

--- date_range/static/src/js/date_range.js
+++ date_range/static/src/js/date_range.js
@@ -2,75 +2,124 @@
  * Copyright 2021 Studio73 - Pablo Fuentes (https://www.studio73.es)
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl). */
 
-/** @odoo-module **/
+odoo.define("date_range.CustomFilterItem", function (require) {
+    "use strict";
 
-import { _t } from "@web/core/l10n/translation";
-import { session } from "@web/session";
-import { FIELD_OPERATORS, FIELD_TYPES } from "@web/search/utils/misc";
-import { CustomFilterItem } from "@web/search/custom_filter_item/custom_filter_item";
-import { patch } from "@web/core/utils/patch";
+    // Try the new imports first, fallback to old ones
+    let _t, session, FIELD_OPERATORS, FIELD_TYPES, CustomFilterItem, patch;
 
-patch(CustomFilterItem.prototype, "date_range.CustomFilterItem", {
-    async setup() {
-        await this._super(...arguments);
-        await this._computeDateRangeOperators();
-    },
+    try {
+        // Odoo 17 style imports
+        _t = require("@web/core/l10n/translation")._t;
+        session = require("@web/session").session;
+        const searchUtils = require("@web/search/utils/misc");
+        FIELD_OPERATORS = searchUtils.FIELD_OPERATORS;
+        FIELD_TYPES = searchUtils.FIELD_TYPES;
+        CustomFilterItem = require("@web/search/custom_filter_item/custom_filter_item").CustomFilterItem;
+        patch = require("@web/core/utils/patch").patch;
+    } catch (e) {
+        // Fallback to old style imports
+        const core = require("web.core");
+        _t = core._t;
+        session = require("web.session");
+        const searchUtils = require("web.searchUtils");
+        FIELD_OPERATORS = searchUtils.FIELD_OPERATORS;
+        FIELD_TYPES = searchUtils.FIELD_TYPES;
+        CustomFilterItem = require("web.CustomFilterItem");
 
-    async _computeDateRangeOperators() {
-        this.OPERATORS = Object.assign({}, FIELD_OPERATORS);
-        this.OPERATORS.date = [...FIELD_OPERATORS.date];
-        this.OPERATORS.datetime = [...FIELD_OPERATORS.datetime];
-        this.date_ranges = {};
+        // For older versions, create a simple patch function
+        patch = function (obj, name, methods) {
+            Object.assign(obj, methods);
+        };
+    }
 
-        const result = await this.env.services.rpc({
-            model: "date.range",
-            method: "search_read",
-            fields: ["name", "type_id", "date_start", "date_end"],
-            context: session.user_context,
-        });
+    // Create the enhancement
+    const DateRangeCustomFilterItem = CustomFilterItem.extend({
+        init: function () {
+            this._super.apply(this, arguments);
+            this._computeDateRangeOperators();
+        },
 
-        result.forEach((range) => {
-            const range_type = range.type_id[0];
-            if (this.date_ranges[range_type] === undefined) {
-                const r = {
-                    symbol: "between",
-                    description: `${_t("in")} ${range.type_id[1]}`,
-                    date_range: true,
-                    date_range_type: range_type,
-                };
-                this.OPERATORS.date.push(r);
-                this.OPERATORS.datetime.push(r);
-                this.date_ranges[range_type] = [];
+        _computeDateRangeOperators: function () {
+            const self = this;
+            this.OPERATORS = Object.assign({}, FIELD_OPERATORS);
+            this.OPERATORS.date = [...FIELD_OPERATORS.date];
+            this.OPERATORS.datetime = [...FIELD_OPERATORS.datetime];
+            this.date_ranges = {};
+
+            return this._rpc({
+                model: "date.range",
+                method: "search_read",
+                fields: ["name", "type_id", "date_start", "date_end"],
+                context: session.user_context,
+            }).then(function (result) {
+                result.forEach(function (range) {
+                    const range_type = range.type_id[0];
+                    if (self.date_ranges[range_type] === undefined) {
+                        const r = {
+                            symbol: "between",
+                            description: _t("in") + " " + range.type_id[1],
+                            date_range: true,
+                            date_range_type: range_type,
+                        };
+                        self.OPERATORS.date.push(r);
+                        self.OPERATORS.datetime.push(r);
+                        self.date_ranges[range_type] = [];
+                    }
+                    self.date_ranges[range_type].push(range);
+                });
+            });
+        },
+
+        _setDefaultValue: function (condition) {
+            const type = this.fields[condition.field].type;
+            const operator = this.OPERATORS[FIELD_TYPES[type]][condition.operator];
+            if (operator && operator.date_range) {
+                const default_range = this.date_ranges[operator.date_range_type][0];
+                // Use moment if available, otherwise fallback to Date
+                if (window.moment) {
+                    const d_start = moment(default_range.date_start + " 00:00:00");
+                    const d_end = moment(default_range.date_end + " 23:59:59");
+                    condition.value = [d_start, d_end];
+                } else {
+                    const d_start = new Date(default_range.date_start + "T00:00:00");
+                    const d_end = new Date(default_range.date_end + "T23:59:59");
+                    condition.value = [d_start, d_end];
+                }
+            } else {
+                this._super.apply(this, arguments);
             }
-            this.date_ranges[range_type].push(range);
-        });
-    },
+        },
 
-    _setDefaultValue(condition) {
-        const type = this.fields[condition.field].type;
-        const operator = this.OPERATORS[FIELD_TYPES[type]][condition.operator];
-        if (operator.date_range) {
-            const default_range = this.date_ranges[operator.date_range_type][0];
-            const d_start = luxon.DateTime.fromISO(`${default_range.date_start}T00:00:00`);
-            const d_end = luxon.DateTime.fromISO(`${default_range.date_end}T23:59:59`);
-            condition.value = [d_start, d_end];
-        } else {
-            this._super(...arguments);
-        }
-    },
+        _onValueInput: function (condition, ev) {
+            const type = this.fields[condition.field].type;
+            const operator = this.OPERATORS[FIELD_TYPES[type]][condition.operator];
+            if (operator && operator.date_range) {
+                const eid = parseInt(ev.target.value);
+                const ranges = this.date_ranges[operator.date_range_type];
+                const range = ranges.find(function (x) { return x.id == eid; });
+                if (range) {
+                    // Use moment if available, otherwise fallback to Date
+                    if (window.moment) {
+                        const d_start = moment(range.date_start + " 00:00:00");
+                        const d_end = moment(range.date_end + " 23:59:59");
+                        condition.value = [d_start, d_end];
+                    } else {
+                        const d_start = new Date(range.date_start + "T00:00:00");
+                        const d_end = new Date(range.date_end + "T23:59:59");
+                        condition.value = [d_start, d_end];
+                    }
+                }
+            } else {
+                this._super.apply(this, arguments);
+            }
+        },
+    });
 
-    _onValueInput(condition, ev) {
-        const type = this.fields[condition.field].type;
-        const operator = this.OPERATORS[FIELD_TYPES[type]][condition.operator];
-        if (operator.date_range) {
-            const eid = parseInt(ev.target.value);
-            const ranges = this.date_ranges[operator.date_range_type];
-            const range = ranges.find((x) => x.id == eid);
-            const d_start = luxon.DateTime.fromISO(`${range.date_start}T00:00:00`);
-            const d_end = luxon.DateTime.fromISO(`${range.date_end}T23:59:59`);
-            condition.value = [d_start, d_end];
-        } else {
-            this._super(...arguments);
-        }
-    },
+    // Try to patch the original class if possible
+    if (patch && CustomFilterItem.prototype) {
+        patch(CustomFilterItem.prototype, "date_range.CustomFilterItem", DateRangeCustomFilterItem.prototype);
+    }
+
+    return DateRangeCustomFilterItem;
 });
