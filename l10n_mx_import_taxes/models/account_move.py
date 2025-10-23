feda65f34d22359ee03a4036dec316f299dd88b1
HBM-GWCG <genaro@gwcg.mx>
2025-10-23 15:38:52 -0600

    [FIX]  l10n_mx_import_taxes

--- l10n_mx_import_taxes/models/account_move.py
+++ l10n_mx_import_taxes/models/account_move.py
@@ -31,7 +31,6 @@ class AccountMove(models.Model):
         caba_account_ids = tax_ids.company_id.account_cash_basis_base_account_id.ids
         res = self.mapped("line_ids").filtered(
             lambda line: not line.tax_ids
-            and line.exclude_from_invoice_tab
             and line.partner_id != line.move_id.partner_id
             and line.account_id.id in caba_account_ids
         )
