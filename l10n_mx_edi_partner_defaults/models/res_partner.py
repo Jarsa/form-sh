1e3a071738f004a0ec88c99dadd596f1c31a8802
HBM-GWCG <genaro@gwcg.mx>
2025-10-23 17:11:28 -0600

    [FIX]  l10n_mx_edi_partner_defaults

--- l10n_mx_edi_partner_defaults/models/res_partner.py
+++ l10n_mx_edi_partner_defaults/models/res_partner.py
@@ -13,13 +13,21 @@ class Partner(models.Model):
         "documents (invoices, payments, and bank statements).",
         default=lambda self: self.env.ref("l10n_mx_edi.payment_method_otros", raise_if_not_found=False),
     )
-
+    def _get_usage_default(self):
+        selection = self._get_usage_selection()
+        # Busca P01 en la selección, si existe
+        for code, label in selection:
+            if code == "P01":
+                return code
+        # Si no existe, regresa el primer valor permitido
+        return selection[0][0] if selection else False
+    
     def _get_usage_selection(self):
         return self.env["account.move"].fields_get().get("l10n_mx_edi_usage").get("selection")
 
     l10n_mx_edi_usage = fields.Selection(
         _get_usage_selection,
         "Usage",
-        default="P01",
+        default=lambda self: self._get_usage_default(),
         help="This usage will be used instead of the default one for invoices.",
     )
