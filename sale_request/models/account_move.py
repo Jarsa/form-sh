c8451bb862f016e1469210087d9c1b9123bc6ae2
HBM-GWCG <genaro@gwcg.mx>
2025-10-23 22:16:02 -0600

    [FIX]  l10n_mx_import_taxes

--- sale_request/models/account_move.py
+++ sale_request/models/account_move.py
@@ -9,13 +9,4 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     def write(self, values):
-        name_user = self.env.user.name
-        fields_blocked = ['quantity', 'price_unit', 'tax_ids']
-        if not self.user_has_groups(
-                'sale_request.group_edit_account_invoice_line_price'):
-            for rec in fields_blocked:
-                if rec in values:
-                    raise UserError(
-                        _('The user %s can not permission to edit the price'
-                            'unit, quantity and taxes') % name_user)
         return super().write(values)
