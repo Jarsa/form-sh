a76648eb977ef6f59df103f31aee4dc72902d562
HBM-GWCG <genaro@gwcg.mx>
2025-10-24 10:51:08 -0600

    [FIX] sale_request | generar out correctamente

--- custom_sale_reason/models/sale_order.py
+++ custom_sale_reason/models/sale_order.py
@@ -3,18 +3,7 @@ from odoo import models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    def action_confirm(self):
-        return {
-            'name':       'Confirmation Reason',
-            'type':       'ir.actions.act_window',
-            'res_model':  'sale.reason.wizard',
-            'view_mode':  'form',
-            'target':     'new',
-            'context': {
-                'default_sale_order_id': self.id,
-                'default_next_state':    'confirmed',
-            }
-        }
+    # override eliminado para no bloquear la confirmación real
 
     def action_cancel(self):
         return {
