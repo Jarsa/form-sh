34ad4f0f5e1f89f9962673309a22d55730e269ca
[HBM] <hbm@gwcg.co>
2026-03-03 13:06:44 -0600

    [FIX] account_move_line_override | quitar bloqueo

--- account_move_line_override/__manifest__.py
+++ account_move_line_override/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Account Move Line Override',
     'version': '1.0',
     'summary': 'Permite eliminar líneas de impuestos en asientos contables',
-    'author': '[HBM] - GW',
+    'author': '[HBM] - GW', 
     'category': 'Accounting',
     'depends': ['account'],
     'data': [],
