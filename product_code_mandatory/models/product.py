4a1f0708ee81bc0586e7dc92155c42855d0f7825
[HBM] <hbm@gwcg.co>
2026-03-19 11:57:09 -0600

    rediseño product code mandatory

--- product_code_mandatory/models/product.py
+++ product_code_mandatory/models/product.py
@@ -1,12 +1,13 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators License AGPL-3.0
 # or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
+    @api.model
     def _get_default_code(self):
         return self.env["ir.sequence"].next_by_code("product.default.code")
 
